deb9648f476a12d5bbb84571d618cae118b714e1
k-mickey <kokmickey@yandex.ru>
2023-11-18 11:58:17 +0300

    Внесены исправления

--- project/settings.py
+++ project/settings.py
@@ -1,31 +1,27 @@
 import os
+import dj_database_url
 
 from environs import Env
 
 env = Env()
 env.read_env()
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'HOST': env.str('HOST'),
-        'PORT': env.str('PORT'),
-        'NAME': env.str('NAME'),
-        'USER': env.str('LOGIN'),
-        'PASSWORD': env.str('PASSWORD'),
-    }
+    'default': dj_database_url.config(
+        default=env.str('DATABASE_URL'),
+        engine='django.db.backends.postgresql_psycopg2',
+    )
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
